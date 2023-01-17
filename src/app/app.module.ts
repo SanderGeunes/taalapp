e3fdcf1c9465475bb964d1c5173737def1576d58
SanderGeunes <sandergeunes@gmail.com>
2023-01-17 14:58:50 +0100

    Added routing manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LidwoordenComponent } from './lidwoorden/lidwoorden.component';
 import { MeerkeuzeComponent } from './meerkeuze/meerkeuze.component';
 import { WoordflitserComponent } from './woordflitser/woordflitser.component';
 import { ResultaatComponent } from './resultaat/resultaat.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'Lidwoorden', component: LidwoordenComponent},
+  {path: 'Meerkeuze', component: MeerkeuzeComponent},
+  {path: 'Woordflitser', component: WoordflitserComponent},
+  {path: 'Resultaat', component: ResultaatComponent}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +24,12 @@ import { ResultaatComponent } from './resultaat/resultaat.component';
     LidwoordenComponent,
     MeerkeuzeComponent,
     WoordflitserComponent,
-    ResultaatComponent
+    ResultaatComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
