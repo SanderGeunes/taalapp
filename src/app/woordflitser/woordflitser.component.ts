1ad0cfb885bf10a128b6c7569076ff30b7ac703e
3lis3 <elise.vanaerschot@hotmail.com>
2023-01-26 00:07:37 +0100

    created first draft js for 'woordflitser' - hard coded

--- src/app/woordflitser/woordflitser.component.ts
+++ src/app/woordflitser/woordflitser.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { Router } from '@angular/router';
+//import { WordsService } from '../words.service';
 
 @Component({
   selector: 'app-woordflitser',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./woordflitser.component.css']
 })
 export class WoordflitserComponent {
+  word: string;
+  translation: string;
 
+  //constructor(private router: Router, private wordsService: WordsService){}
+
+  showWords(){
+  this.word = "Hallo";
+  this.translation = "Bonjour";
+
+  console.log(this.word);
+
+  setTimeout(() => {
+    console.log(this.translation);
+  }, 5000);
+  }
 }
