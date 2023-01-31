9aebe6598c9c9517357a880abeaacf0a4cf9cda6
LanderPoelmans <LanderPoelmansgit@gmail.com>
2023-01-31 17:34:31 +0100

    made vocabulaire + update on meerkeuze

---
+++ src/app/vocabulaire/vocabulaire.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { WordsService } from '../words.service';
+import { generate } from 'rxjs';
+@Component({
+  selector: 'app-vocabulaire',
+  templateUrl: './vocabulaire.component.html',
+  styleUrls: ['./vocabulaire.component.css']
+})
+export class VocabulaireComponent {
+  constructor(private wordService: WordsService) {
+
+   }
+   words: any;
+   category: any;
+   length: any;
+   ngOnInit() {
+    let category = sessionStorage.getItem("category");
+    this.wordService.getWords(parseFloat(category)).then(data => {
+      console.log(data);
+      this.length = data.length;
+      this.words = data;
+      console.log(this.words);
+    })
+    }
+  }
